c07653ec066c95705eb8475ec72ad1c3248ef008
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-10-23 11:29:50 +0300

    lists

---
+++ src/lists.kt
@@ -0,0 +1,10 @@
+fun main() {
+//    Immutable lists
+    val constantList = listOf("Python", "Kotlin", "Flutter")
+    val changableList = mutableListOf("Denzel", "Gitonga", "Catherine")
+
+    print(constantList)
+    print(" ")
+    println(changableList)
+
+}
