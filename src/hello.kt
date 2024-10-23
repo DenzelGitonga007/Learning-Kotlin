c103cccfb1958701b3bc298332360e560b84988d
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-10-23 10:26:43 +0300

    variables

--- src/hello.kt
+++ src/hello.kt
@@ -1,3 +1,8 @@
 fun main() {
     println("Hello world, Denzel is learning Kotlin")
+    println("My name is $myName, and my phone number is $myPhone")
+
 }
+//Declaring variables
+val myName = "Denzel"
+var myPhone = 714082283
