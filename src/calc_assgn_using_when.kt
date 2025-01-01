e800ddf1f38dd986fdfdd0313052d25c66dba2c1
DenzelGitonga007 <gitongadenzel@gmail.com>
2025-01-01 14:10:01 +0300

    inputs with operators

---
+++ src/calc_assgn_using_when.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+// Calculator assignment using when
+
+fun main() {
+    println("....Welcome to Denzel Calculator....")
+    println("....Powered by Kotlin....")
+
+//    Get the user input, two numbers
+    var reader = Scanner(System.`in`)
+    println("...To perform a calculation...")
+    println("Enter the first number: ")
+    var num1 = reader.nextInt() // assign the first input to the the reader
+    println("Enter the second number:")
+
+//    Get the operation to be done on the numbers
+    var operator = readLine()
+}
