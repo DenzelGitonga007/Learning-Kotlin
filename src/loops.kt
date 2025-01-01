81819cefc759d4598e393a08b1f6c98490966455
DenzelGitonga007 <gitongadenzel@gmail.com>
2025-01-01 15:22:56 +0300

    do while loop

--- src/loops.kt
+++ src/loops.kt
@@ -1,14 +1,35 @@
 fun main() {
 
 //    While loop
+//    Incrementing
+
 //    Assign first limit
     var lower_limit = 0 // initialize the lower limit
 
 //    Set the condition to determine the range to the upper limit
-
     while (lower_limit <= 40) {
         println("The number is $lower_limit")
         lower_limit++
     }
+
+//    Decrementing
+    var upper_limit = 10
+
+    while (upper_limit>=0) {
+        println("The number is $upper_limit")
+        upper_limit --
+    }
+
+//    Do while
+    var counter=31
+
+    do {
+        println(counter)
+        counter++
+    }
+    while (counter<=25)
+
+//    For loop
+
 
 }
