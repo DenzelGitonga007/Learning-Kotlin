863c1890dea836be6efaecf12e072ea70f0523d1
DenzelGitonga007 <gitongadenzel@gmail.com>
2025-01-01 15:14:52 +0300

    while loop

---
+++ src/loops.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+//    While loop
+//    Assign first limit
+    var lower_limit = 0 // initialize the lower limit
+
+//    Set the condition to determine the range to the upper limit
+
+    while (lower_limit <= 40) {
+        println("The number is $lower_limit")
+        lower_limit++
+    }
+
+}
